7a7d2b517fc72c1201daf2ecc633c820ce2a6498
Martin Grewe <grewe@zib.de>
2018-01-24 12:09:51 +0100

    fix for paths containing spaces

--- extract_pages_from_pdf.sh
+++ extract_pages_from_pdf.sh
@@ -11,7 +11,7 @@ fi
 FROM=$(awk -F, '{print $1}' <<<$OUTPUT)
 TO=$(awk -F, '{print $2}' <<<$OUTPUT)
 
-NEW_FILENAME=$(basename $1 .pdf)_extracted.pdf
+NEW_FILENAME=$(basename "$1" .pdf)_extracted.pdf
 OUTPUT=$(zenity --file-selection --save --title="Save to new pdf" --filename=$NEW_FILENAME)
 accepted=$?
 if ((accepted != 0)); then
@@ -31,7 +31,7 @@ fi
 
 #/usr/bin/gs -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -dFirstPage=$FROM -dLastPage=$TO -sOutputFile=$FILE $1 2>&1 | xargs -I %  zenity --width=250 --height=250 --info --text=%
 
-CMD_OUT=$(/usr/bin/gs -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -dFirstPage=$FROM -dLastPage=$TO -sOutputFile=$FILE $1)
+CMD_OUT=$(/usr/bin/gs -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -dFirstPage=$FROM -dLastPage=$TO -sOutputFile="$FILE" "$1")
 if (($? != 0))
 then
         zenity --error --title "Ghostscript error" --text="$CMD_OUT"
