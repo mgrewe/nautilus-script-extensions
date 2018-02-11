ebf60fd52eb4e2c00a29b787a5c0563546434341
Martin Grewe <grewe@zib.de>
2018-02-11 10:12:06 +0100

    preset option added

--- compress_pdf.sh
+++ compress_pdf.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+opt1="screen"
+opt2="ebook"
+opt3="printer"
+opt4="prepress"
+opt5="default"
+PRESET=$(zenity --height=275 --list --radiolist --text 'Select the preset to be used:' --column 'Selection' --column 'Preset' FALSE "$opt1" FALSE "$opt2" FALSE "$opt3" TRUE "$opt4" FALSE "$opt5")
+if [ -z $PRESET ]
+then
+    exit -1
+fi
+
+
+FILE=$OUTPUT
 
 NEW_FILENAME=$(basename "$1" .pdf)_compressed.pdf
 OUTPUT=$(zenity --file-selection --save --title="Save to new pdf" --filename=$NEW_FILENAME)
@@ -21,7 +34,7 @@ fi
 
 #/usr/bin/gs -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -dFirstPage=$FROM -dLastPage=$TO -sOutputFile=$FILE $1 2>&1 | xargs -I %  zenity --width=250 --height=250 --info --text=%
 
-CMD_OUT=$(/usr/bin/gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$FILE" "$1")
+CMD_OUT=$(/usr/bin/gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/$PRESET -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$FILE" "$1")
 if (($? != 0))
 then
         zenity --error --title "Ghostscript error" --text="$CMD_OUT"
