fff69daffeba5e0dcb82da2104cd181f7ca525d8
Martin Grewe <grewe@zib.de>
2018-02-13 20:24:48 +0100

    respects multiple files now for pdf merging

--- compress_pdf.sh
+++ compress_pdf.sh
@@ -31,10 +31,16 @@ then
         fi
 fi
 
+FILES=""
+ 
+while (( "$#" )); do 
+  FILES="$FILES $(printf '%q' "$1")"
+  shift 
+done
 
 #/usr/bin/gs -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -dFirstPage=$FROM -dLastPage=$TO -sOutputFile=$FILE $1 2>&1 | xargs -I %  zenity --width=250 --height=250 --info --text=%
 
-CMD_OUT=$(/usr/bin/gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/$PRESET -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$FILE" "$1")
+CMD_OUT=$(/usr/bin/gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/$PRESET -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$FILE" $FILES)
 if (($? != 0))
 then
         zenity --error --title "Ghostscript error" --text="$CMD_OUT"
